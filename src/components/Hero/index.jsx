c4bd3ffa4e71a6baffa498aca257e943605fbc65
nodirbek27 <nodirjon0927@gmail.com>
2024-10-11 21:38:18 +0500

    add Hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,30 +1,47 @@
-
 const Hero = () => {
-    return (
-        <section id="home" className="">
-            <div>
-                <div className="">
-                    <div>
-                        <figure className="">
-                            <img src="" alt="Nodirbek Nurmamatov portrait" width={40} height={40} className="img-cover" />
-                        </figure>
-                        <div className="">
-                            <span className="">
-                                <span></span>
-                            </span>
-                            Available for work
-                        </div>
-                    </div>
-                    <h2>Building Scalable Modern Websites for the Future</h2>
-                    <div className="">
-                        ButtonPrimary
+  return (
+    <section id="home" className="pt-28 lg:pt-36">
+      <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
 
-                        ButtonOutline
-                    </div>
-                </div>
+        <div className="">
+          <div className="flex items-center gap-3">
+            <figure className="img-box w-9 h-9 rounded-lg">
+              <img
+                src=""
+                alt="Nodirbek Nurmamatov portrait"
+                width={40}
+                height={40}
+                className="img-cover"
+              />
+            </figure>
+            <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+              <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
+              </span>
+              Available for work
             </div>
-        </section>
-    )
-}
+          </div>
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+            Building Scalable Modern Websites for the Future
+          </h2>
+          <div className="flex items-center gap-3">ButtonPrimary ButtonOutline</div>
+        </div>
+
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-sky-400/40 to-[65%] rounded-[65px] overflow-hidden">
+            <img
+              src=""
+              alt="Nodirbek Nurmamatov"
+              width={656}
+              height={800}
+              className="w-full"
+            />
+          </figure>
+        </div>
+
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
